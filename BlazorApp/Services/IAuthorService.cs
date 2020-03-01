c46189ac4f5857127b0fd60d60bd0391cb9899e9
unknown <aqil_y@etna-alternance.com>
2020-03-01 15:00:09 +0100

    commit all

---
+++ BlazorApp/Services/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+    interface IAuthorService
+    {
+        Task<List<Author>> GetAuthors();
+        Task<Author> GetAuthorById(int authorId);
+        Task<bool> SaveAuthor(Author author);
+        Task<bool> DeleteAuthor(string authorId);
+        DateTime GetCreateDate();
+        string GetVersion();
+    }
+}
---
+++ BlazorApp/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿//function saveMessage() {
+
+//   // alert(firstName + ' ' + lastName +' Record has been saved successfully');
+
+//    document.getElementById('divServerValidation').innerHTML = 'Record has been saved successfully'
+//}
+
+function setFocusOnElement(element) {
+    element.focus();
+}
+
+function getCities() {
+
+    //throw 'Something has gone wrong JS';
+
+    var cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio',
+        'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'Fort Worth', 'Columbus', 'San Francisco',
+        'Charlotte', 'Indianapolis', 'Seattle', 'Denver', 'Washington'];
+    return cities;
+}
